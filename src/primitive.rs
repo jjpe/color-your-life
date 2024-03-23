868a616d50293ff4b47c0c01f52b4fae701230e5
Joey Ezechiels <joey.ezechiels@gmail.com>
2024-03-24 00:33:11 +0100

    primitive: Generate wrapper types that implement std::fmt::Display

--- src/primitive.rs
+++ src/primitive.rs
@@ -1,22 +1,19 @@
 //!
 
-use crate::{ColorDisplay, Style};
-use std::fmt::Write;
+use crate::{Color, ColorDisplay, Style};
+use std::fmt::{Alignment, Write};
 
-macro_rules! impl_ColorDisplay_for_numeric_types {
-    (
-        $([impl ColorDisplay<$argtype:ident> for $type:ty]),*
-            $(,)?
-    ) => {
+macro_rules! impl_ColorDisplay_and_add_wrappers_for_numeric_types {
+    ($($type:ty),* $(,)?) => { paste::paste! {
         $(
-            impl ColorDisplay<$argtype> for $type {
+            impl ColorDisplay<[<$type:camel Format>]> for $type {
                 fn color_fmt(
                     &self,
                     sink: &mut impl Write,
-                    pargs: &$argtype,
+                    format: &[<$type:camel Format>],
                 ) -> std::fmt::Result {
-                    self.write_indentation(sink, pargs.indent, pargs)?;
-                    if let Some(desc) = pargs.style {
+                    self.write_indentation(sink, format.indent, format)?;
+                    if let Some(desc) = format.style {
                         let style = desc.color.normal();
                         let style = if desc.bold      { style.bold()      } else { style };
                         let style = if desc.italic    { style.italic()    } else { style };
@@ -29,27 +26,50 @@ macro_rules! impl_ColorDisplay_for_numeric_types {
                 }
             }
 
-            pub struct $argtype {
+            pub struct [<$type:camel Format>] {
                 pub indent: u16,
                 pub style: Option<Style>,
             }
+
+            impl [<$type:camel Format>] {
+                pub fn standard(indent: u16) -> Self {
+                    Self {
+                        indent,
+                        style: Some(Style {
+                            color: Color::Blue,
+                            bold: false,
+                            italic: false,
+                            underline: false,
+                            dimmed: false,
+                        }),
+                    }
+                }
+            }
+
+            pub struct [<$type:camel DisplayWrapper>](pub $type);
+
+            impl std::fmt::Display for [<$type:camel DisplayWrapper>] {
+                fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                    if let (Some(Alignment::Right), Some(width)) =
+                        (f.align(), f.width())
+                    {
+                        let indent: u16 = width.try_into()
+                            .map_err(|_| std::fmt::Error)?;
+                        let format = [<$type:camel Format>]::standard(indent);
+                        self.0.color_fmt(f, &format)
+                    } else {
+                        Err(std::fmt::Error)
+                    }
+                }
+            }
         )*
-    }
+    }}
 }
 
-impl_ColorDisplay_for_numeric_types! {
-    [impl ColorDisplay<I8Format> for i8],
-    [impl ColorDisplay<I16Format> for i16],
-    [impl ColorDisplay<I32Format> for i32],
-    [impl ColorDisplay<I64Format> for i64],
-    [impl ColorDisplay<I128Format> for i128],
-    [impl ColorDisplay<IsizeFormat> for isize],
-    [impl ColorDisplay<U8Format> for u8],
-    [impl ColorDisplay<U16Format> for u16],
-    [impl ColorDisplay<U32Format> for u32],
-    [impl ColorDisplay<U64Format> for u64],
-    [impl ColorDisplay<U128Format> for u128],
-    [impl ColorDisplay<UsizeFormat> for usize],
+impl_ColorDisplay_and_add_wrappers_for_numeric_types! {
+    i8, i16, i32, i64, i128, isize,
+    u8, u16, u32, u64, u128, usize,
+    f32, f64,
 }
 
 #[cfg(test)]
@@ -63,7 +83,7 @@ mod test {
                 $(
                     #[test]
                     fn [<basic_ $type:lower>]() -> std::fmt::Result {
-                        let num = 42;
+                        let num = 42 as $type;
                         let mut sink = String::with_capacity(1024);
                         num.color_fmt(&mut sink, &[<$type:camel Format>] {
                             indent: 0,
@@ -89,5 +109,15 @@ mod test {
     generate_tests_for_numeric_types! {
         i8, i16, i32, i64, i128, isize,
         u8, u16, u32, u64, u128, usize,
+        f32, f64,
+    }
+
+    #[test]
+    fn display_u8() -> std::fmt::Result {
+        let num = 42;
+        let displayed = format!("{:>2}", U8DisplayWrapper(num));
+        let expected = format!("        {}", Color::Blue.paint("42"));
+        assert_eq!(displayed, expected);
+        Ok(())
     }
 }
